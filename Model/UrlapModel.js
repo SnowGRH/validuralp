481551517ee671de4e2f3480b48d0b54e6d99f34
SnowGRH <gergelyovinczeff@gmail.com>
2023-10-24 15:41:58 +0200

    asdasd

--- Model/UrlapModel.js
+++ Model/UrlapModel.js
@@ -1,11 +1,11 @@
-import { Urlapleiro } from "./Uralpleiro.js"
-class UrlapModel{
-    #leiro={}
-    constructor(){
-this.#leiro = Urlapleiro;
-    }
-    get leiro(){
-        return {...this.#leiro};
-    }
+import { Urlapleiro } from "./Uralpleiro.js";
+class UrlapModel {
+  #leiro = {};
+  constructor() {
+    this.#leiro = Urlapleiro;
+  }
+  get leiro() {
+    return { ...this.#leiro };
+  }
 }
-export default UrlapModel
+export default UrlapModel;
